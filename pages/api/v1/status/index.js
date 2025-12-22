3ea36d5fc147e1095feed57b42eac7ae1a3630de
Pedro Teixeira <pedrops02@gmail.com>
2025-12-22 19:36:15 -0300

    adiciona configuração do banco de dados PostgreSQL e implementa consulta de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1+1 AS result");
+  console.log(result);
   res.status(200).json({ status: "ok" });
 }
 
