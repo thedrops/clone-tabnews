d0853348a2b7c4d2ad5c433c85771c7523e615f7
Pedro Teixeira <pedrops02@gmail.com>
2026-01-06 22:05:59 -0300

    add jsconfig.json and update import path in status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(req, res) {
   const result = await database.query("SELECT 1+1 AS result");
-  console.log(result);
+  console.log(result.rows);
   res.status(200).json({ status: "ok" });
 }
 
